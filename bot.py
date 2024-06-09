25af869b29e65df1ddef0e29452e488180ef9668
kamalsiraj <padoraemon8@gmail.com>
2024-06-09 19:17:56 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from main import genn_pass
+
+#membaca token dari file token.txt
+with open("token.txt", "r") as f:
+    token = f.read()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def heh(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def passw(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run(token)
